33962e17931e6d4efae2fdca8f59b9b251a5ced1
Ughur Alakbarov <ugur.alekperov@gmail.com>
2025-01-02 17:23:28 +0100

    feat(ParsedMerge): remove `create_merge`
    It was only ever used in one place that's now been removed.
    Additionally, I think it's not helpful as a general method, since
    1. it asserts `has_additional_conflicts=false` (which we can't really
        know without running `line_based_with_duplicate_signature_detection`)
    2. it marks `method="original"`, which was really only useful in the
        context of `resolve_merge_cascading`

--- src/lib.rs
+++ src/lib.rs
@@ -483,7 +483,17 @@ pub fn resolve_merge_cascading<'a>(
                 }
                 Err(err) => warn!("{err}"),
             }
-            merges.push(parsed_merge.to_merge_result(&settings));
+
+            let rendered_from_parsed = {
+                MergeResult {
+                    contents: parsed_merge.render(&settings),
+                    conflict_count: parsed_merge.conflict_count(),
+                    conflict_mass: parsed_merge.conflict_mass(),
+                    method: "original",
+                    has_additional_issues: false,
+                }
+            };
+            merges.push(rendered_from_parsed);
         }
     }
 
