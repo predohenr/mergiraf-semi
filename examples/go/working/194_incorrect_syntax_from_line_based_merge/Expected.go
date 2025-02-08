5ba07838ba479472cf237ce4256a85416a3bc95b
Ada Alakbarova <ada.alakbarova@proton.me>
2025-02-08 15:52:05 +0100

    add failing test
    minimized from `util/redisutil/redisutil.go`

---
+++ examples/go/working/194_incorrect_syntax_from_line_based_merge/Expected.go
@@ -0,0 +1,30 @@
+<<<<<<< LEFT
+import "github.com/redis/go-redis/v9"
+||||||| BASE
+import "github.com/go-redis/redis/v8"
+=======
+import (
+	"fmt"
+	"net"
+	"net/url"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+>>>>>>> RIGHT
+
+// RedisClientFromURL creates a new Redis client based on the provided URL.
+// The URL scheme can be either `redis` or `redis+sentinel`.
+func RedisClientFromURL(url string) (redis.UniversalClient, error) {
+	if url == "" {
+		return nil, nil
+	}
+	redisOptions, err := redis.ParseURL(url)
+	if err != nil {
+		return nil, err
+	}
+	return redis.NewClient(redisOptions), nil
+}
