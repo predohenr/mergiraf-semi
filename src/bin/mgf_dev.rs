354088e3b89a688a47ea6ce74efb25e39815c4ad
Antonin Delpeuch <antonin@delpeuch.eu>
2025-02-11 18:53:24 +0100

    feat: Add command to compare trees. Fixes #197.

--- src/bin/mgf_dev.rs
+++ src/bin/mgf_dev.rs
@@ -27,6 +27,13 @@ enum Command {
         /// Path to the file to parse. Its type will be guessed from its extension.
         path: PathBuf,
     },
+    /// Compare two files, returning exit code 0 if their trees are isomorphic, and 1 otherwise
+    Compare {
+        /// Path to the first file
+        first: PathBuf,
+        /// Path to the second file
+        second: PathBuf,
+    },
 }
 
 fn main() {
@@ -47,6 +54,7 @@ fn real_main(args: &CliArgs) -> Result<i32, String> {
 
     let language_determining_path = match &args.command {
         Command::Parse { path } => path,
+        Command::Compare { first, .. } => first,
     };
 
     let lang_profile =
@@ -75,11 +83,94 @@ fn real_main(args: &CliArgs) -> Result<i32, String> {
             print!("{}", tree.root().ascii_tree(lang_profile));
             Ok(0)
         }
+        Command::Compare { first, second } => {
+            let original_contents_first = fs::read_to_string(first)
+                .map_err(|err| format!("Could not read {}: {err}", first.display()))?;
+            let contents_first = normalize_to_lf(original_contents_first);
+
+            let ts_tree_first = parser
+                .parse(&*contents_first, None)
+                .ok_or("Parsing failed")?;
+            let tree_first = Ast::new(
+                &ts_tree_first,
+                &contents_first,
+                lang_profile,
+                &arena,
+                &ref_arena,
+            )
+            .map_err(|err| format!("File has parse errors: {err}"))?;
+
+            let original_contents_second = fs::read_to_string(second)
+                .map_err(|err| format!("Could not read {}: {err}", first.display()))?;
+            let contents_second = normalize_to_lf(original_contents_second);
+
+            let ts_tree_second = parser
+                .parse(&*contents_second, None)
+                .ok_or("Parsing failed")?;
+            let tree_second = Ast::new(
+                &ts_tree_second,
+                &contents_second,
+                lang_profile,
+                &arena,
+                &ref_arena,
+            )
+            .map_err(|err| format!("File has parse errors: {err}"))?;
+
+            Ok(if tree_first.root().isomorphic_to(tree_second.root()) {
+                0
+            } else {
+                1
+            })
+        }
     }
 }
 
-#[test]
-fn verify_cli() {
-    use clap::CommandFactory;
-    CliArgs::command().debug_assert();
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_cli() {
+        use clap::CommandFactory;
+        CliArgs::command().debug_assert();
+    }
+
+    #[test]
+    fn test_isomorphism_identical_files() {
+        assert_eq!(
+            real_main(&CliArgs::parse_from([
+                "mgf_dev",
+                "compare",
+                "examples/java/working/demo/Base.java",
+                "examples/java/working/demo/Base.java",
+            ])),
+            Ok(0)
+        );
+    }
+
+    #[test]
+    fn test_isomorphism_isomorphic_trees() {
+        assert_eq!(
+            real_main(&CliArgs::parse_from([
+                "mgf_dev",
+                "compare",
+                "examples/java/working/reformat/Base.java",
+                "examples/java/working/reformat/Left.java",
+            ])),
+            Ok(0)
+        );
+    }
+
+    #[test]
+    fn test_isomorphism_different_trees() {
+        assert_eq!(
+            real_main(&CliArgs::parse_from([
+                "mgf_dev",
+                "compare",
+                "examples/java/working/demo/Base.java",
+                "examples/java/working/demo/Left.java",
+            ])),
+            Ok(1)
+        );
+    }
 }
