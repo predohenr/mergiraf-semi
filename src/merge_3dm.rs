5ce9e301ddc41fd933899ad89392a24930cb5c58
Ada Alakbarova <ada.alakbarova@proton.me>
2025-02-22 15:07:25 +0100

    refactor(line_based_merge): make `DisplaySettings` non-`Option`al
    This effectively reverts #176
    
    It turns out we only have two places where it was called with
    `settings=None`
    1) The line-based fallback pipeline -- which we now know was a mistake.
    2) One random test -- we can just pass a default value here.
       In fact, we use `settings` later in that same test,
       so create a variable to reuse.

--- src/merge_3dm.rs
+++ src/merge_3dm.rs
@@ -298,8 +298,7 @@ fn build_tree<'a>(
         settings,
     );
     let merged_tree = tree_builder.build_tree().unwrap_or_else(|_| {
-        let line_based =
-            line_based_merge(base.source(), left.source(), right.source(), Some(settings));
+        let line_based = line_based_merge(base.source(), left.source(), right.source(), settings);
         MergedTree::LineBasedMerge {
             node: class_mapping.map_to_leader(RevNode::new(Revision::Base, base.root())),
             contents: line_based.contents,
