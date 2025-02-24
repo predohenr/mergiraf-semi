1145e9e2c409d7a1e61e829442dd0a3f4f164307
Ada Alakbarova <ada.alakbarova@proton.me>
2025-02-24 18:36:12 +0100

    test(spork/conflicting_variable_rename): fix expected compact conflict
    the markers should be at the beginning of the corresponding line
    
    NOTE: the space should ideally be placed outside the conflict, since
    it's present in all revisions, but that's not done currently it seems. I
    just went with the current output since it's not too important

--- examples/spork/failing/conflicting_variable_rename/Expected.java
+++ examples/spork/failing/conflicting_variable_rename/Expected.java
@@ -1,21 +1,21 @@
 public class Adder {
     public int add(int lhs, int
-    <<<<<<< LEFT
-    rhs
-    ||||||| BASE
-    b
-    =======
-    r
-    >>>>>>> RIGHT
+<<<<<<< LEFT
+ rhs
+||||||| BASE
+ b
+=======
+ r
+>>>>>>> RIGHT
     ) {
         return lhs +
-        <<<<<<< LEFT
-        rhs
-        ||||||| BASE
-        b
-        =======
-        r
-        >>>>>>> RIGHT
+<<<<<<< LEFT
+ rhs
+||||||| BASE
+ b
+=======
+ r
+>>>>>>> RIGHT
         ;
     }
 }
