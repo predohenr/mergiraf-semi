54fb7923f0ca1e6efd1b2ca302a350e4312dad22
Antonin Delpeuch <antonin@delpeuch.eu>
2025-03-07 08:23:01 +0100

    fix: Make Python "import from" statements commutative

---
+++ examples/python/working/import_from/Left.py
@@ -0,0 +1,18 @@
+from .....pagination import SyncCursorPage, AsyncCursorPage
+from ....._base_client import AsyncPaginator, make_request_options
+from .....lib.streaming import (
+    AssistantEventHandler,
+    AssistantEventHandlerT,
+    AssistantStreamManager,
+    AsyncAssistantEventHandler,
+    AsyncAssistantEventHandlerT,
+    AsyncAssistantStreamManager,
+)
+from .....types.chat_model import ChatModel
+from .....types.beta.threads import (
+    run_list_params,
+    run_create_params,
+    run_update_params,
+    run_submit_tool_outputs_params,
+)
+
---
+++ examples/python/working/import_from/Right.py
@@ -0,0 +1,9 @@
+from .....pagination import SyncCursorPage, AsyncCursorPage
+from ....._base_client import AsyncPaginator, make_request_options
+from .....types.beta.threads import (
+    run_list_params,
+    run_create_params,
+    run_update_params,
+    run_submit_tool_outputs_params,
+)
+
