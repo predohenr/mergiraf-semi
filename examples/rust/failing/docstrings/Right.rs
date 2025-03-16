eb29febed7c63967fa52c61f668dd21b72074d76
Ada Alakbarova <ada.alakbarova@proton.me>
2025-03-16 17:55:06 +0100

    minimize repro
    the contents of the function and the module don't change, so we can
    treat them as atomic nodes for the purposes of this test.

--- examples/rust/failing/docstrings/Right.rs
+++ examples/rust/failing/docstrings/Right.rs
@@ -1,21 +1,2 @@
-/// Escapes `"` or `\` with `\` for use in AppleScript text
-fn esc_quote(s: &str) -> Cow<'_, str> {
-    if s.contains(&['"', '\\']) {
-        let mut r = String::with_capacity(s.len());
-        let chars = s.chars();
-        for c in chars {
-            match c {
-                '"' | '\\' => {
-                    r.push('\\');
-                    r.push(c);
-                } // escapes quote/escape char
-                _ => {
-                    r.push(c);
-                } // no escape required
-            }
-        }
-        Cow::Owned(r)
-    } else {
-        Cow::Borrowed(s)
-    }
-}
+/// comment
+fn foo() {}
