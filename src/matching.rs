c75e7a0980685d02dea1ff16fa20fac8293e420e
Ada Alakbarova <ada.alakbarova@proton.me>
2025-03-18 18:55:40 +0100

    perf: use `HashMap::with_capacity`
    I wish the `_and_hasher` part was necessary (since we know we'll be
    using `FxHasher`), but oh well

--- src/matching.rs
+++ src/matching.rs
@@ -1,5 +1,5 @@
-use rustc_hash::{FxHashMap, FxHashSet};
-use std::collections::HashSet;
+use rustc_hash::{FxBuildHasher, FxHashMap, FxHashSet};
+use std::collections::{HashMap, HashSet};
 
 use crate::tree::AstNode;
 
@@ -84,14 +84,18 @@ impl<'tree> Matching<'tree> {
     /// 1. reverse the first one to get a left-base matching
     /// 2. compose that with the second one: (left -> base), (base -> right) => (left -> right)
     pub fn compose_base_left_and_base_right(base_left: &Self, base_right: &Self) -> Self {
-        let mut left_to_right = FxHashMap::default();
-        let mut right_to_left = FxHashMap::default();
+        let mut left_to_right =
+            HashMap::with_capacity_and_hasher(base_left.right_to_left.len(), FxBuildHasher);
+        let mut right_to_left =
+            HashMap::with_capacity_and_hasher(base_right.right_to_left.len(), FxBuildHasher);
+
         for (source, target) in base_left.iter_right_to_left() {
             if let Some(final_target) = base_right.get_from_left(target) {
                 left_to_right.insert(*source, final_target);
                 right_to_left.insert(final_target, *source);
             }
         }
+
         Self {
             left_to_right,
             right_to_left,
