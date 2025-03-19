8a4a11458509dedf6a9967c745659004307bf85a
Ada Alakbarova <ada.alakbarova@proton.me>
2025-03-19 11:54:03 +0100

    fmt `rust/move_to_method`
    this, perhaps obviously, does not correspond to output of rustfmt

--- examples/rust/working/move_to_method/Right.rs
+++ examples/rust/working/move_to_method/Right.rs
@@ -1,8 +1,9 @@
-
 impl Lawnmower {
     fn find_home_station(&self) -> Option<&Station> {
-        self.neighbouring_stations()
-            .iter()
-            .find(|station| station.id == "home" && !station.occupied && station.color == StationColor::Blue)
+        self.neighbouring_stations().iter().find(|station| {
+            station.id == "home"
+                && !station.occupied
+                && station.color == StationColor::Blue
+        })
     }
 }
