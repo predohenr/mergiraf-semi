2553cfb90ef322a63f38643ed278d9ebf2288db0
Ben Boeckel <ben.boeckel@kitware.com>
2025-03-28 21:05:18 +0100

    fix: explicitly declare that `LangProfile` is `'static`
    Lifetime elision otherwise ties the lifetime to the input `filename`
    lifetime which is not true.

--- src/lang_profile.rs
+++ src/lang_profile.rs
@@ -32,7 +32,7 @@ pub struct LangProfile {
 
 impl LangProfile {
     /// Detects the language of a file based on its filename
-    pub fn detect_from_filename<P>(filename: &P) -> Option<&Self>
+    pub fn detect_from_filename<P>(filename: &P) -> Option<&'static Self>
     where
         P: AsRef<Path> + ?Sized,
     {
@@ -40,7 +40,7 @@ impl LangProfile {
         Self::_detect_from_filename(filename)
     }
 
-    fn _detect_from_filename(filename: &Path) -> Option<&Self> {
+    fn _detect_from_filename(filename: &Path) -> Option<&'static Self> {
         // TODO make something more advanced like in difftastic
         // https://github.com/Wilfred/difftastic/blob/master/src/parse/tree_sitter_parser.rs
         let extension = filename.extension()?;
