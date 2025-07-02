519908c225afa90acd7323e022c4f8fd929096ff
Antonin Delpeuch <antonin@delpeuch.eu>
2025-07-02 15:02:23 +0200

    refactor: Change detected test extension to be a suffix
    So that the empty string (without a dot) can also be an
    acceptable suffix.

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -26,17 +26,21 @@ pub(crate) fn write_file_from_rev(
     repo_dir: &Path,
     test_dir: &Path,
     revision: &str,
-    extension: &str,
+    suffix: &str,
 ) -> PathBuf {
-    let file_name = format!("file.{extension}");
-    let fname_base = test_dir.join(format!("{revision}.{extension}"));
+    let file_name = format!("file{suffix}");
+    let fname_base = test_dir.join(format!("{revision}{suffix}"));
     let contents = fs::read_to_string(&fname_base).expect("Unable to read left file");
     fs::write(repo_dir.join(&file_name), contents)
         .expect("failed to write test file to git repository");
     PathBuf::from(file_name)
 }
 
-pub(crate) fn detect_extension(test_dir: &Path) -> String {
+/// Detect the suffix (including period) used by the revision files in a test case,
+/// if any. Test files without extensions should declare the language to use in
+/// a separate `language` directory and just use bare `Base`, `Left` and `Right`
+/// revision files (and similarly for expected outputs).
+pub(crate) fn detect_test_suffix(test_dir: &Path) -> String {
     read_dir(test_dir)
         .expect("Could not list files in test directory")
         .find_map(|filename| {
@@ -48,5 +52,6 @@ pub(crate) fn detect_extension(test_dir: &Path) -> String {
                 .strip_prefix("Base.")
                 .map(String::from)
         })
-        .expect("Could not find a Base.* file in the test directory")
+        .map(|ext| format!(".{ext}"))
+        .unwrap_or_else(|| "".to_owned())
 }
--- tests/working.rs
+++ tests/working.rs
@@ -8,7 +8,7 @@ use mergiraf::{PathBufExt, line_merge_and_structured_resolution};
 use rstest::rstest;
 
 mod common;
-use common::detect_extension;
+use common::detect_test_suffix;
 
 fn compare_against_merge(
     test_dir: &Path,
@@ -49,23 +49,23 @@ fn compare_against_merge(
 }
 
 fn run_test_from_dir(test_dir: &Path) {
-    let ext = detect_extension(test_dir);
+    let suffix = detect_test_suffix(test_dir);
     #[expect(unstable_name_collisions)]
-    let fname_base = test_dir.join(format!("Base.{ext}")).leak();
+    let fname_base = test_dir.join(format!("Base{suffix}")).leak();
     let contents_base = fs::read_to_string(&fname_base)
         .expect("Unable to read left file")
         .leak();
-    let fname_left = test_dir.join(format!("Left.{ext}"));
+    let fname_left = test_dir.join(format!("Left{suffix}"));
     let contents_left = fs::read_to_string(fname_left)
         .expect("Unable to read left file")
         .leak();
-    let fname_right = test_dir.join(format!("Right.{ext}"));
+    let fname_right = test_dir.join(format!("Right{suffix}"));
     let contents_right = fs::read_to_string(fname_right)
         .expect("Unable to read right file")
         .leak();
 
     {
-        let fname_expected = test_dir.join(format!("Expected.{ext}"));
+        let fname_expected = test_dir.join(format!("Expected{suffix}"));
         let contents_expected =
             fs::read_to_string(fname_expected).expect("Unable to read expected file");
 
@@ -82,7 +82,7 @@ fn run_test_from_dir(test_dir: &Path) {
 
     {
         // only run the following part if the file exists
-        let fname_expected_compact = test_dir.join(format!("ExpectedCompact.{ext}"));
+        let fname_expected_compact = test_dir.join(format!("ExpectedCompact{suffix}"));
         let Ok(contents_expected_compact) = fs::read_to_string(fname_expected_compact) else {
             return;
         };
