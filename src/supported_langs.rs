027a42fd558c2ba21885a372ff726b8115b9a65f
Ada Alakbarova <ada.alakbarova@proton.me>
2025-07-14 21:52:10 +0200

    make the test actually fail

--- src/supported_langs.rs
+++ src/supported_langs.rs
@@ -1089,6 +1089,6 @@ mod test {
                 }
             }
         }
-        eprintln!("\ntotal invalid children: {count}");
+        assert_eq!(count, 0, "\ntotal invalid children: {count}");
     }
 }
